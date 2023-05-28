4919213d3eb4c4b5e6a044f2499a25b403e7b76f
fedek6 <fedek6@gmail.com>
2023-05-28 12:18:02 +0200

    Update backup script

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@
 current_dir=${PWD##*/}
 
 # Create a tarball, excluding node_modules directories
-tar -czvf "../${current_dir}.tar.gz" --exclude=node_modules ./*
+tar -czvf "../${current_dir}.tar.gz" --exclude=node_modules --exclude=uploads ./*
